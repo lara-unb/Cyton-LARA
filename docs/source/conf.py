99b58fec75ee10349185d54135abeec4f41ea104
Rafael Lima <akafael@users.noreply.github.com>
2016-12-20 14:37:31 -0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,8 +34,7 @@ import sys
 extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
-    'sphinx.ext.mathjax',
-    'sphinx.ext.githubpages']
+    'sphinx.ext.mathjax']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
